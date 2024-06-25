53f0b9fd72afe8f316336f1fe599767266a93c37
Luiz101516 <162833326+Luiz101516@users.noreply.github.com>
2024-06-25 19:47:14 -0300

    Teste alterando a route do index para deploy

--- exercicioUm/index.py
+++ exercicioUm/index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/templates/")
 def main():
     return render_template("index.html")
 
